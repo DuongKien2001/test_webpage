384701bb6dcc487d89c497a01a8ce2467bb213e1
DuongKien2001 <74645733+DuongKien2001@users.noreply.github.com>
2022-11-15 21:34:10 +0700

    Add files via upload

--- index1.php
+++ index1.php
@@ -4,6 +4,24 @@
         <title>PHP Form</title>
     </head>
     <body>
+        <?php
+        function check_infor($fn, $ls, $ea, $os){
+            if ($ea == "") return 1;
+            else if ($ls == "") return 2;
+            else return false;
+        }
+        ?>
+        <?php
+        $first_name = $_POST["fname"];
+        $last_name = $_POST["lname"];
+        $email_address = $_POST["email"];
+        $os = $_POST["os"];
+        $result = check_infor($first_name, $last_name, $email_address, $os);
+        if ($result == 1) echo"<p style=\"color:red\">"."Email Address is empty"."</p>";
+        else if ($result == 2) echo "<p style=\"color:red\">"."Last Adress is empty"."</p>";
+        else echo "<p style=\"color:red\">"."Sign in Successfully!"."</p>"
+
+        ?>
         <form method="post">
             <p><b>Please enter your information</b></p>
             <p>Fields with a * are required</p>
